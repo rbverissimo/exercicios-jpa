08c5b5e910788c52f2e56612044492fb68fb05a7
rbverissimo <rverissimo.dev@gmail.com>
2023-02-02 18:01:40 -0300

    Criada a classe Produto e devidamente implementada

---
+++ src/main/java/modelo/basico/Produto.java
@@ -0,0 +1,51 @@
+package modelo.basico;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	
+	private Double preco;
+	
+	
+	//O construtor padrão é necessário para funcionamento do JPA
+	public Produto() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	
+	
+
+}
