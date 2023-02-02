c5bee14ab13767e96e62e8859680463e3d64a3da
rbverissimo <rverissimo.dev@gmail.com>
2023-02-02 12:13:46 -0300

    Modificação nas classes e criação da classe ObterUsuario

--- src/main/java/teste/basico/ObterUsuario.java
+++ src/main/java/teste/basico/ObterUsuario.java
@@ -6,25 +6,20 @@ import javax.persistence.Persistence;
 
 import modelo.basico.Usuario;
 
-public class NovoUsuario {
+public class ObterUsuario {
 	
+
 	public static void main(String[] args) {
 		
 		EntityManagerFactory emf = Persistence
 				.createEntityManagerFactory("exercicios-jpa");
 		EntityManager em = emf.createEntityManager();
 		
-		Usuario novoUsuario = new Usuario("Renato", "renato@lanche.com.br");
+		Usuario usuario = em.find(Usuario.class, 1L);
+		System.out.println(usuario.getNome());
 		
-		em.getTransaction().begin();
-		em.persist(novoUsuario);
-		em.getTransaction().commit();
-		
-		System.out.println("O ID gerado foi: " + novoUsuario.getId());
-
 		em.close();
 		emf.close();
 		
 	}
-
 }
