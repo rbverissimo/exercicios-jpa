fdf79971f61d3860fae3f5529138de559c2d3548
rbverissimo <rverissimo.dev@gmail.com>
2023-02-02 12:05:03 -0300

    Atualização na classe novoUsuario

--- src/main/java/teste/basico/NovoUsuario.java
+++ src/main/java/teste/basico/NovoUsuario.java
@@ -14,10 +14,14 @@ public class NovoUsuario {
 				.createEntityManagerFactory("exercicios-jpa");
 		EntityManager em = emf.createEntityManager();
 		
-		Usuario novoUsuario = new Usuario("Djalma", "djalma@lanche.com.br");
+		Usuario novoUsuario = new Usuario("Ronaldo", "ronaldo@lanche.com.br");
+		Usuario novoUsuario2 = new Usuario("Edivaldo", "edivaldo@lanche.com.br");
+		Usuario novoUsuario3 = new Usuario("Cleiton", "cleiton@lanche.com.br");
 		
 		em.getTransaction().begin();
 		em.persist(novoUsuario);
+		em.persist(novoUsuario2);
+		em.persist(novoUsuario3);
 		em.getTransaction().commit();
 
 		em.close();
