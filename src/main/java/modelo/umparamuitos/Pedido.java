6d864dee99a73e711f3783006dec0878500dbc8f
rbverissimo <rverissimo.dev@gmail.com>
2023-02-03 14:12:22 -0300

    Adição das classes ItemPedido e Pedido para se comunicarem em uma relação OneToMany envolvendo também o produto. Ou seja, um Produto pode estar em vários ItemPedidos, porém um ItemPedido pode estar em apenas um pedido que pode conter vários ItemPedidos diferentes

---
+++ src/main/java/modelo/umparamuitos/Pedido.java
@@ -0,0 +1,36 @@
+package modelo.umparamuitos;
+
+import java.util.Date;
+
+public class Pedido {
+	
+	private Long id;
+	
+	private Date data;
+	
+	public Pedido() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+	
+	
+	
+	
+	
+
+}
