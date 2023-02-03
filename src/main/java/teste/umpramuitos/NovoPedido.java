66d8cda8a981160997561a1202fa3a960f319876
rbverissimo <rverissimo.dev@gmail.com>
2023-02-03 14:42:11 -0300

    Implementado a lógica criação do Pedido no banco de dados

--- src/main/java/teste/umpramuitos/NovoPedido.java
+++ src/main/java/teste/umpramuitos/NovoPedido.java
@@ -2,6 +2,7 @@ package teste.umpramuitos;
 
 import infra.DAO;
 import modelo.basico.Produto;
+import modelo.umpramuitos.ItemPedido;
 import modelo.umpramuitos.Pedido;
 
 public class NovoPedido {
@@ -12,7 +13,15 @@ public class NovoPedido {
 		
 		Pedido pedido = new Pedido();
 		Produto produto = new Produto("Geladeira", 2789.99);
+		ItemPedido item = new ItemPedido(pedido, produto, 10); 
 		
+	
+		dao .abrirT()
+			.incluir(produto)
+			.incluir(pedido)
+			.incluir(item)
+			.fecharT()
+			.fechar();
 		
 	}
 
