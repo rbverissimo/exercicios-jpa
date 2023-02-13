f8ee980b92d0cc9d7d6305ab11463d3422d3bc1a
rbverissimo <rverissimo.dev@gmail.com>
2023-02-13 11:58:10 -0300

    Criação da SQL para uso como Named Native Query no consultas.xml. Criação das classes que manipularão a consulta.

---
+++ src/main/java/teste/consultas/ObterMediaFilmes.java
@@ -0,0 +1,15 @@
+package teste.consultas;
+
+import infra.DAO;
+import modelo.muitospramuitos.Filme;
+
+public class ObterMediaFilmes {
+	
+	public static void main(String[] args) {
+		
+		DAO<Filme> filmes = new DAO<>(Filme.class);
+		
+		
+	}
+
+}
