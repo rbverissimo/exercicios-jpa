6b7ddba09b80576b1ade37c47dc50495348527b1
rbverissimo <rverissimo.dev@gmail.com>
2023-02-14 11:56:42 -0300

    Adicionando às classes Aluno e AlunoBolsista o tratamento de herança feito pelo JPA através das annotations @Inheritance @DiscriminatorColumn e @DiscriminatorValue

--- src/main/java/modelo/heranca/Aluno.java
+++ src/main/java/modelo/heranca/Aluno.java
@@ -1,9 +1,18 @@
 package modelo.heranca;
 
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
 @Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "tipo", length = 2, 
+discriminatorType = DiscriminatorType.STRING )
+@DiscriminatorValue("AL")
 public class Aluno {
 	
 	@Id
--- src/main/java/modelo/heranca/AlunoBolsista.java
+++ src/main/java/modelo/heranca/AlunoBolsista.java
@@ -1,8 +1,10 @@
 package modelo.heranca;
 
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
+@DiscriminatorValue("AB")
 public class AlunoBolsista extends Aluno {
 	
 	private Double valorBolsa;
